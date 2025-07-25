f21f237ffac6bf7e132c5a2499aebe653183eaea
Cristiano Coffy <cristianocoffy@gmail.com>
2025-07-25 19:25:47 -0300

    Estrutura inicial projeto

---
+++ backend/FinboostPlus/src/main/java/com/finboostplus/FinboostPlusApplication.java
@@ -0,0 +1,13 @@
+package com.finboostplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinboostPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinboostPlusApplication.class, args);
+	}
+
+}
---
+++ backend/FinboostPlus/src/test/java/com/finboostplus/FinboostPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finboostplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinboostPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
