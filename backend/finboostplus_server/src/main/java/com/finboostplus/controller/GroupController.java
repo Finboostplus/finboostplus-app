40c55f6e393f925379dd93c6206ef10b72c1b95d
cristiano <cristianocoffy@gmail.com>
2025-09-06 17:21:26 -0300

    Ajuste espaços

--- backend/finboostplus_server/src/main/java/com/finboostplus/controller/GroupController.java
+++ backend/finboostplus_server/src/main/java/com/finboostplus/controller/GroupController.java
@@ -93,15 +93,7 @@ public class GroupController {
     }
 
 
-    @GetMapping("/{groupId}/expenses")
-    public ResponseEntity<Object> listDetailsGroup(@PathVariable Long groupId) {
-        GroupDetailsDTO dto = groupService.getExpenseGroupById(groupId);
-        if (dto != null) {
-            return ResponseEntity.ok().body(dto);
-        }
-        return ResponseEntity.badRequest().body("Não foi possivel obter os detalhes do grupo");
 
-    }
     @GetMapping("/{groupId}/members")
     public ResponseEntity<List<GroupMemberResponseDTO>> findAllMembersByGroupId(@PathVariable long groupId) {
         return new ResponseEntity<>(groupService.findAllMembersByGroupId(groupId), HttpStatus.OK);
