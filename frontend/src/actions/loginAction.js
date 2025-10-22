d766d75755f26ea0d6848144de14b7685fe9c669
Cleyton Jesus <cleytonjesus007@gmail.com>
2025-10-21 23:47:27 -0300

    fix:removendo login desnecessário

--- frontend/src/actions/loginAction.js
+++ frontend/src/actions/loginAction.js
@@ -26,7 +26,6 @@ export const loginAction = async ({ request }) => {
 
     return { success, errors, value: data };
   }
-  login(data.email, data.password);
   /* Se não ocorreu nenhum erro */
   /* Logar aqui */
 
