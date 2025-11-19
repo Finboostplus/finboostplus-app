dc768586f98d74d203cb395cfa23adac3e9e4f98
Jetulio <79411834+jetchaolin@users.noreply.github.com>
2025-11-19 10:56:30 -0300

    Enable scheduling in FinboostplusApplication

--- backend/finboostplus_server/src/main/java/com/finboostplus/FinboostplusApplication.java
+++ backend/finboostplus_server/src/main/java/com/finboostplus/FinboostplusApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.finboostplus.repository.GroupMemberRepository;
 import com.finboostplus.repository.GroupRepository;
